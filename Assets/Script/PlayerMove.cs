40d210bc59d611991c5b353e3c75f43fb65f6a33
HwangJaeHo <hjh222427@gmail.com>
2020-06-02 23:33:31 +0900

    [기능] Player Object 생성 및 Player 조작 기능 추가
    1. Player Object 생성 후 물리현상 추가 및 Object 회전 제한
    
    2. PlayerMove.cs를 통한 Player 조작 기능 추가
    
    3. Main Camera를 Player에 고정

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+
+    Rigidbody rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        //Stop Character
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.velocity = new Vector3(rigid.velocity.normalized.x * 0.5f, 
+                rigid.velocity.y, rigid.velocity.z);
+        if (Input.GetButtonUp("Vertical"))
+            rigid.velocity = new Vector3(rigid.velocity.x, 
+                rigid.velocity.y, rigid.velocity.normalized.z * 0.5f);
+    }
+
+    void FixedUpdate()
+    {
+        //Move by Key Control
+        float x = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(Vector3.forward * x, ForceMode.Impulse);
+        float y = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector3.right * y, ForceMode.Impulse);
+
+        //Max Speed
+        if (rigid.velocity.x > maxSpeed)
+            rigid.velocity = new Vector3(maxSpeed, rigid.velocity.y, rigid.velocity.z);
+        if (rigid.velocity.x < -maxSpeed)
+            rigid.velocity = new Vector3(-maxSpeed, rigid.velocity.y, rigid.velocity.z);
+        if (rigid.velocity.z > maxSpeed)
+            rigid.velocity = new Vector3(rigid.velocity.x, rigid.velocity.y, maxSpeed);
+        if (rigid.velocity.z < -maxSpeed)
+            rigid.velocity = new Vector3(rigid.velocity.x, rigid.velocity.y, -maxSpeed);
+    }
+}
